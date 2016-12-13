e7ed3b479f1d4b952d62048106277110723367cb
Steven Gonzalez <stevenrgon@hotmail.com>
2016-12-13 16:36:55 -0500

    implementing routing

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,5 +1,4 @@
 angular.module('newsPage', [])
-
     // inject the Todo service factory into our controller
     .controller('mainController', ['$scope', '$http', function ($scope, $http) {
         $scope.test = function () {
--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,14 @@
-angular.module('newsPage');
+var app = angular.module('newsPage',['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "views/index.html"
+    })
+    .when("/about", {
+        templateUrl : "views/about.html"
+    })
+    .when("/contact", {
+        templateUrl : "views/contact.html"
+    })
+});
+
