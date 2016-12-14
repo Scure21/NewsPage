8bb12be75b5ae82b7ddb712c1d8b41af7c1a6bf3
Steven Gonzalez <stevenrgon@hotmail.com>
2016-12-14 17:00:44 -0500

    added toolbar and menu

--- app/public/js/controllers/nav-bar-controller.js
+++ app/public/js/controllers/nav-bar-controller.js
@@ -1,6 +1,15 @@
 angular.module('newsPage')
     .controller('navCtrl', navCtrl);
 
-function navCtrl($scope) {
+function navCtrl($scope, $mdSidenav) {
     $scope.currentNavItem = 'page1';
+
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+
+    function buildToggler(componentId) {
+        return function () {
+            $mdSidenav(componentId).toggle();
+        }
+    }
 }
